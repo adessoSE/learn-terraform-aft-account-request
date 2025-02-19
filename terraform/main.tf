c2f06ae57d21ab0f94f76b0a0e0b5ba342fba832
BekirKocabas <bekirkocabas49@gmail.com>
2025-02-19 15:07:18 +0100

    Create sandbox-aft Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "bekir.kocabas@adesso.de"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "bekir.kocabas@adesso.de"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
