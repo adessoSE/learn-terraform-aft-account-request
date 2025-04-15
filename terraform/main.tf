dfcecd714fd8a4d2a95d16f0f6c158ef79704ef5
BekirKocabas <bekirkocabas49@gmail.com>
2025-04-15 12:48:55 +0200

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,30 +26,3 @@ module "sandbox" {
   account_customizations_name = "sandbox"
 }
 
-module "sandbox" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "bekir.kocabas@adesso.de"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "bekir.kocabas@adesso.de"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
-}
