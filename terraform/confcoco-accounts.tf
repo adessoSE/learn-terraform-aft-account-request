31e2c8e967685c25419ebd6c72b4f0978097ec6c
BekirKocabas <bekirkocabas49@gmail.com>
2025-04-15 17:01:34 +0200

    add repo Account

--- terraform/confcoco-accounts.tf
+++ terraform/confcoco-accounts.tf
@@ -26,3 +26,32 @@ module "confcoco-management-account" {
   account_customizations_name = "confcoco"
 
 }
+
+module "confcoco-repo-account" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "itmc-conf-comp-repo@adesso.de"
+    AccountName               = "confcoco-repo-account"
+    ManagedOrganizationalUnit = "confcoco"
+    SSOUserEmail              = "itmc-conf-comp-repo@adesso.de"
+    SSOUserFirstName          = "confcoco-repo"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "Name" = "confcoco-repo-account"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Bekir Kocabas"
+    change_reason       = "We need a repo account for confcoco"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "confcoco"
+
+}
